ff914982dd89d030b225cfc7dac386cbe7bc28d4
codertosh <156106056+codertosh@users.noreply.github.com>
2025-02-07 23:41:14 +0600

    Update Voltage-esp32-1.ino

--- Voltage-esp32-1.ino
+++ Voltage-esp32-1.ino
@@ -1,13 +1,16 @@
 
+
+
+
 // https://diyprojectslab.com/interfacing-zmpt101b-voltage-sensor-with-esp32/
 
 #include <Wire.h>
 #include <ZMPT101B.h>
 
 #define SENSITIVITY 500.0f       // Adjust this value to calibrate sensitivity
-#define CALIBRATION_FACTOR 1.293  // Calibration factor to correct the reading (230 / 155)
+#define CALIBRATION_FACTOR 1.52  // Calibration factor to correct the reading (230 / 155)
 
-ZMPT101B voltageSensor(34, 50.0);
+ZMPT101B voltageSensor(27, 50.0);
 
 void setup() {
   Serial.begin(115200);
